76670c8d691e56afb79c08a0730187ba141f5c9e
Ashton <AshtonTech10@gmail.com>
2025-11-08 14:45:24 -0600

    Added ingredient and sales file processing and put them into usuable format

---
+++ sale_processing.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import re
+
+def process_sale_data(path:str) -> tuple:
+    sheet_names = ["data 1","data 2", "data 3"]
+    all_data_raw = "" #Kind of unhinged, but puts all the product data into one colossal string
+    total_sales_raw = "" #Puts all of the sales data into one colossal string
+    for sheet in sheet_names:
+        temp_data = pd.read_excel(path,sheet_name=sheet)
+        str_data = temp_data.to_string()
+        if "Group" in str_data:
+            total_sales_raw += str_data
+        else:
+            all_data_raw += str_data
+    all_data_raw = re.sub(r"\s+(source_page)\s+(source_table).+","",all_data_raw)
+    total_sales_raw = re.sub(r"\s+(source_page)\s+(source_table).+","",total_sales_raw)
+    return make_dictionary(total_sales_raw),make_dictionary(all_data_raw)
+
+def make_dictionary(raw_data:str) -> dict:
+    rows = raw_data.split('\n')
+    data_for_items = []
+    for row in rows:
+        if row == "":
+            continue
+        data_for_item = {}
+        info = re.split(pattern=r"\s{2,}(?![\(-])",string=row)
+        data_for_item["name"] = info[3]
+        data_for_item["count"] = int(re.sub(r"[$,]","",info[4]))
+        data_for_item["amount"] = float(re.sub(r"[$,]","",info[5]))
+        data_for_items.append(data_for_item)
+    return data_for_items
+
+
+data_for_items = process_sale_data("mai-shen-yun-main\October_Data_Matrix_20251103_214000.xlsx")
+print(data_for_items[1])
---
+++ shipment_processing.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import re
+
+def process_shipment_data(path:str) -> dict:
+    ingredients_data = []
+    all_data_raw = ""
+    temp_data = pd.read_csv(path)
+    all_data_raw = temp_data.to_string()
+    rows = all_data_raw.split("\n")
+    rows.pop(0)
+    shipment_amount = {"weekly":4,"biweekly":8,"monthly":1}
+    for row in rows:
+        temp = re.split(pattern=r"\s{2,}",string=row)
+        ingredient_data = {}
+        ingredient_data["ingredient"] = temp[1]
+        ingredient_data["unit_of_shipment"] = temp[3]
+        ingredient_data["amount_per_month"] = int(temp[2])*int(temp[4])*shipment_amount[temp[5].lower()]
+        ingredients_data.append(ingredient_data)
+    return ingredients_data
+print(process_shipment_data("mai-shen-yun-main\\MSY Data - Shipment.csv"))
